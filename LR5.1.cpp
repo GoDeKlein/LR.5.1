91c80bb7bb79a17c7b8890680bcd2f3bbaa4872c
GoDeKlein <tkachartyom51@gmail.com>
2020-11-04 02:31:18 +0300

    x змынили на i

--- LR5.1.cpp
+++ LR5.1.cpp
@@ -7,15 +7,15 @@ using namespace std;
 
 int main()
 {
-    double a, b, x, y, z;
-    printf("Input x: ");
-    scanf_s("%lf", &x);
+    double a, b, i, y, z;
+    printf("Input i: ");
+    scanf_s("%lf", &i);
     printf("Input y: ");
     scanf_s("%lf", &y);
     printf("Input z: ");
     scanf_s("%lf", &z);
-    a = ((2 * cos(x - (M_PI / 6))) / (1 / 2 + pow(sin(y), 2))) + cos(154 * M_PI / 180);
-    b = 1 + (pow(z, 2) / (abs(3 - y) + pow(z, 2) / 5)) + pow(x - 5 * y, 1 / 3) + exp(2 * x - y);
+    a = ((2 * cos(i - (M_PI / 6))) / (1 / 2 + pow(sin(y), 2))) + cos(154 * M_PI / 180);
+    b = 1 + (pow(z, 2) / (abs(3 - y) + pow(z, 2) / 5)) + pow(i - 5 * y, 1 / 3) + exp(2 * x - y);
     printf("Answer\n", &a);
     printf("a=%lf\n", a);
     printf("Answer\n", &b);
